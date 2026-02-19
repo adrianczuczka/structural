90d8520b4927f5a8d398b1368fb6f3df05b749bc
Adrian Czuczka <adrianczuczka@gmail.com>
2026-02-19 10:34:10 +0000

    Increase version to 1.1.0

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("io.deepmedia.tools.deployer") version "0.17.0"
 }
 
-val releaseVersion = "1.0.1"
+val releaseVersion = "1.1.0"
 
 group = "com.adrianczuczka"
 version = releaseVersion
