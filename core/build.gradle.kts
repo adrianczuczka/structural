38d0b7ee34e59edd6a54046d77d3851ecff8aa9e
Adrian Czuczka <adrianczuczka@gmail.com>
2026-08-10 12:41:43 +0100

    Bump version to 2.0.0-beta1

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("io.deepmedia.tools.deployer") version "0.17.0"
 }
 
-val releaseVersion = "1.2.0"
+val releaseVersion = "2.0.0-beta1"
 
 group = "com.adrianczuczka"
 version = releaseVersion
